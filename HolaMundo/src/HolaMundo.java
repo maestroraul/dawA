4a01c8709c4c475984b4bea1bf5720367460c00b
raul <raul.ruiz11@educa.madrid.org>
2022-04-01 13:16:06 +0200

    Cambio Eclipse

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -3,7 +3,8 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		
-         System.out.println("Hola Mundo");
+         System.out.println("Hola Mundo-cambio Consola");
+         System.out.println("Hola Mundo-cambio Eclipse");
 	}
 
 }
