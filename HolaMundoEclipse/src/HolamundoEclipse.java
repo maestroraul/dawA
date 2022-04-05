6a3259d9def6a7a649dc8000f8846f65b303f584
raul <raul.ruiz11@educa.madrid.org>
2022-04-05 15:40:36 +0200

    Cambio desde Eclipse

---
+++ HolaMundoEclipse/src/HolamundoEclipse.java
@@ -0,0 +1,15 @@
+
+public class HolamundoEclipse {
+	
+	
+	
+
+		public static void main(String[] args) {
+			
+	        
+	         System.out.println("Hola Mundo eclipse-cambio desde Eclipse 1");
+	         
+
+	}
+
+}
