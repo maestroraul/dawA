9af8861f7cd56146d5b07ff96ef3e9978fb637d1
raul <raul.ruiz11@educa.madrid.org>
2022-04-05 17:21:37 +0200

    Commit prueba Netbeans colaborativa

---
+++ src/main/java/com/mycompany/holamundo4/HolaMundo4.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.holamundo4;
+
+/**
+ *
+ * @author raul
+ */
+public class HolaMundo4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo 4 daw A");
+        ut.println("Hola Mundo 4 daw A v2");
+    }
+}
